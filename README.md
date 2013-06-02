69930d9d00f504bedddbee8e007935abb67fb3b5
Michael Labieniec <michaellabieniec@gmail.com>
2013-06-02 19:11:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### goo.gl url shortener for compoundjs ###
+Just a simple wrapper for https://github.com/kaimallea/node-googl for compoundjs
---
+++ index.js
@@ -0,0 +1,9 @@
+var googl = require('goo.gl');
+    googl.setKey('AIzaSyDUMOv5rdKk7UNRkg1DEvUExvrlodro8wU');
+
+exports.init = function init(compound) {
+
+    var app = compound.app;
+    compound.googl = googl;
+
+};
